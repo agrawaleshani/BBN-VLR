6b98434117682dab660cf65d50d0409730db7e17
Sanjana Gunna <sanjana.gunna99@gmail.com>
2023-04-21 22:01:14 -0400

    Update modelNet.py

--- lib/dataset/modelNet.py
+++ lib/dataset/modelNet.py
@@ -3,24 +3,147 @@ import torch_geometric.transforms as T
 import torchvision
 
 class IMBALANCEMODELNET10():
-    def __init__(self):
-        self.cls_num = 10
+    def __init__(self, mode, cfg, root='./datasets/imbalance_cifar10', imb_type='exp',
+                 transform=None, target_transform=None, download=True):
+        self.train = True if mode == "train" else False
+        super(IMBALANCECIFAR10, self).__init__(root, train, transform, target_transform, download)
+        self.cfg = cfg
+        self.cls_num = cfg.DATASET.CLASSES
+        self.transform = T.SamplePoints(2048)
+        self.pre_transform = T.NormalizeScale()
+        if self.train:
+            dataset = ModelNet(root="ModelNet10", name='10', train=True, transform=self.transform, pre_transform=self.pre_transform)
+        else:
+            dataset = ModelNet(root="ModelNet10", name='10', train=False, transform=self.transform, pre_transform=self.pre_transform)
+        self.data = dataset
+        print("{} Mode: Contain {} images".format(mode, len(self.data)))
+        if self.dual_sample or (self.cfg.TRAIN.SAMPLER.TYPE == "weighted sampler" and self.train):
+            self.class_weight, self.sum_weight = self.get_weight(self.get_annotations(), self.cls_num)
+            self.class_dict = self._get_class_dict()
     
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        elif imb_type == 'step':
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max))
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max * imb_factor))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def sample_class_index_by_weight(self):
+        rand_number, now_sum = random.random() * self.sum_weight, 0
+        for i in range(self.cls_num):
+            now_sum += self.class_weight[i]
+            if rand_number <= now_sum:
+                return i
+            
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        if self.cfg.TRAIN.SAMPLER.TYPE == "weighted sampler" and self.train:
+            assert self.cfg.TRAIN.SAMPLER.WEIGHTED_SAMPLER.TYPE in ["balance", "reverse"]
+            if  self.cfg.TRAIN.SAMPLER.WEIGHTED_SAMPLER.TYPE == "balance":
+                sample_class = random.randint(0, self.cls_num - 1)
+            elif self.cfg.TRAIN.SAMPLER.WEIGHTED_SAMPLER.TYPE == "reverse":
+                sample_class = self.sample_class_index_by_weight()
+            sample_indexes = self.class_dict[sample_class]
+            index = random.choice(sample_indexes)
+
+        img, target = self.data[index], self.targets[index]
+        meta = dict()
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+
+        if self.dual_sample:
+            if self.cfg.TRAIN.SAMPLER.DUAL_SAMPLER.TYPE == "reverse":
+                sample_class = self.sample_class_index_by_weight()
+                sample_indexes = self.class_dict[sample_class]
+                sample_index = random.choice(sample_indexes)
+            elif self.cfg.TRAIN.SAMPLER.DUAL_SAMPLER.TYPE == "balance":
+                sample_class = random.randint(0, self.cls_num-1)
+                sample_indexes = self.class_dict[sample_class]
+                sample_index = random.choice(sample_indexes)
+            elif self.cfg.TRAIN.SAMPLER.DUAL_SAMPLER.TYPE == "uniform":
+                sample_index = random.randint(0, self.__len__() - 1)
+
+            sample_img, sample_label = self.data[sample_index], self.targets[sample_index]
+            meta['sample_image'] = sample_img
+            meta['sample_label'] = sample_label
+
+        return img, target, meta
+
+    def _get_class_dict(self):
+        class_dict = dict()
+        for i, anno in enumerate(self.get_annotations()):
+            cat_id = anno["category_id"]
+            if not cat_id in class_dict:
+                class_dict[cat_id] = []
+            class_dict[cat_id].append(i)
+        return class_dict
+
+    def get_weight(self, annotations, num_classes):
+        num_list = [0] * num_classes
+        cat_list = []
+        for anno in annotations:
+            category_id = anno["category_id"]
+            num_list[category_id] += 1
+            cat_list.append(category_id)
+        max_num = max(num_list)
+        class_weight = [max_num / i for i in num_list]
+        sum_weight = sum(class_weight)
+        return class_weight, sum_weight
     
     def get_num_classes(self):
         return self.cls_num
-    
-    
+   
+    def reset_epoch(self, epoch):
+        self.epoch = epoch
 
+    def get_annotations(self):
+        annos = []
+        for target in self.targets:
+            annos.append({'category_id': int(target)})
+        return annos
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+        # np.random.shuffle(classes)
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+        
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
 
 if __name__ == '__main__':
-    pre_transform = T.NormalizeScale()
-    transform = T.SamplePoints(2048)         
-    trainset = IMBALANCEMODELNET10(root='ModelNet10', train=True,
-                                 transform=transform, pre_transform=pre_transform)
-    train_dataset = ModelNet(root="ModelNet10", name='10', train=True, transform=transform, pre_transform=pre_transform)
-    val_dataset = ModelNet(root="ModelNet10", name='10', train=False, transform=transform, pre_transform=pre_transform)
-
+    trainset = IMBALANCEMODELNET10(root='ModelNet10', train=True)
     trainloader = iter(trainset)
     data, label = next(trainloader)
-    import pdb; pdb.set_trace()
